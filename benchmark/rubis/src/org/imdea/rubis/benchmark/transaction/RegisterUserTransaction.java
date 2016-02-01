be0abdb5cb594b583412e617ed3b6837101e911a
Massimo Neri <hello@mneri.me>
2016-02-01 09:37:37 +0100

    Bugfixing

--- benchmark/rubis/src/org/imdea/rubis/benchmark/transaction/RegisterUserTransaction.java
+++ benchmark/rubis/src/org/imdea/rubis/benchmark/transaction/RegisterUserTransaction.java
@@ -28,54 +28,85 @@ import fr.inria.jessy.transaction.ExecutionHistory;
 import java.util.Date;
 
 import org.imdea.rubis.benchmark.entity.IndexEntity;
+import org.imdea.rubis.benchmark.entity.RegionEntity;
 import org.imdea.rubis.benchmark.entity.UserEntity;
 import org.imdea.rubis.benchmark.exception.UnaccessibleIndexException;
 
 public class RegisterUserTransaction extends AbsRUBiSTransaction {
-    private UserEntity mUser;
+    private float mBalance;
+    private Date mCreationDate;
+    private String mEmail;
+    private String mFirstname;
+    private long mId;
+    private String mLastname;
+    private String mNickname;
+    private String mPassword;
+    private int mRating;
+    private String mRegion;
+    private long mRegionId;
 
     public RegisterUserTransaction(Jessy jessy, long id, String firstname, String lastname, String nickname, String
-            password, String email, int rating, float balance, Date creationDate, long region) throws
+            password, String email, String region) throws Exception {
+        this(jessy, id, firstname, lastname, nickname, password, email, 0, 0.0f, new Date(), region);
+    }
+
+    public RegisterUserTransaction(Jessy jessy, long id, String firstname, String lastname, String nickname, String
+            password, String email, int rating, float balance, Date creationDate, String region) throws
             Exception {
         super(jessy);
+
+        mId = id;
+        mFirstname = firstname;
+        mLastname = lastname;
+        mNickname = nickname;
+        mPassword = password;
+        mEmail = email;
+        mRating = rating;
+        mBalance = balance;
+        mCreationDate = creationDate;
+        mRegion = region;
+
         putExtra(SPSI.LEVEL, SPSI.SER);
-        mUser = new UserEntity(id, firstname, lastname, nickname, password, email, rating, balance, creationDate,
-                region);
     }
 
     private void createNeededIndexeEntities() {
-        // TODO: This sucks. For less spaghetti code index entities should be created on the fly, when needed, but the
-        // TODO: actual code of Jessy doesn't allow this: reading a non-existent org.imdea.benchmark.rubis.entity will increase the fail read
-        // TODO: count (after retrying 10 times) and a lot of code should be changed in order to avoid this.
-        long id = mUser.getId();
-        createIndex(bids.user_id).justEmpty().forKey(id);
-        createIndex(buy_now.buyer_id).justEmpty().forKey(id);
-        createIndex(comments.from_user_id).justEmpty().forKey(id);
-        createIndex(comments.to_user_id).justEmpty().forKey(id);
-        createIndex(items.seller).justEmpty().forKey(id);
-        // TODO: For the same reason it is not convenient to create an index on the pair <nickname, password>. If there
-        // TODO: is a mismatch, Jessy will try 10 times before giving up. D:
-        createIndex(users.nickname).withPointer(id).forKey(mUser.getNickname());
+        createIndex(bids.user_id).justEmpty().forKey(mId);
+        createIndex(buy_now.buyer_id).justEmpty().forKey(mId);
+        createIndex(comments.from_user_id).justEmpty().forKey(mId);
+        createIndex(comments.to_user_id).justEmpty().forKey(mId);
+        createIndex(items.seller).justEmpty().forKey(mId);
+        createIndex(users.nickname).withPointer(mId).forKey(mNickname);
     }
 
     @Override
     public ExecutionHistory execute() {
         try {
-            IndexEntity nickIndex = null;
+            mRegionId = -1;
 
-            // TODO: Same problem here, if the nickname does not exist (which is very likely in the benchmark) Jessy
-            // TODO: will try to read the index 10 times.
-            try {
-                nickIndex = readIndex(users.nickname).find(mUser.getNickname());
-            } catch (UnaccessibleIndexException ignored) {
-                // TODO: In the actual implementation when the index does not exist (i.e. the nickname is not in the
-                // TODO: database) an UnaccessibleIndexException is thrown. So nickIndex remains null.
+            for (long regionId : readScannerOf(regions).getPointers()) {
+                RegionEntity region = readEntityFrom(regions).withKey(regionId);
+
+                if (region.getName().equals(mRegion))
+                    mRegionId = regionId;
             }
 
-            if (nickIndex == null || nickIndex.getPointers().size() == 0) {
-                create(mUser);
-                createNeededIndexeEntities();
-                updateIndexes();
+            if (mRegionId != -1) {
+                IndexEntity nickIndex = null;
+
+                try {
+                    nickIndex = readIndex(users.nickname).find(mNickname);
+                } catch (UnaccessibleIndexException ignored) {
+                    // In the actual implementation when the index does not exist (i.e. the nickname is not in the
+                    // database) an UnaccessibleIndexException is thrown. So nickIndex remains null.
+                }
+
+                if (nickIndex == null || nickIndex.getPointers().size() == 0) {
+                    UserEntity user = new UserEntity(mId, mFirstname, mLastname, mNickname, mPassword, mEmail, mRating,
+                            mBalance, mCreationDate, mRegionId);
+                    create(user);
+                    createNeededIndexeEntities();
+                    updateIndexes();
+                }
             }
 
             return commitTransaction();
@@ -87,7 +118,7 @@ public class RegisterUserTransaction extends AbsRUBiSTransaction {
     }
 
     private void updateIndexes() {
-        IndexEntity regionIndex = readIndex(users.region).find(mUser.getRegion());
-        regionIndex.edit().addPointer(mUser.getId()).write(this);
+        IndexEntity regionIndex = readIndex(users.region).find(mRegionId);
+        regionIndex.edit().addPointer(mId).write(this);
     }
 }
