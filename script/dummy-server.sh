040ab4970c81afb2f7ded1c1803454cfd785ff37
Massimo Neri <hello@mneri.me>
2016-02-26 15:48:46 +0100

    Added shell scripts for dummy and rubis benchmarks

---
+++ script/dummy-server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /home/mneri/Progetti/jessy/script/configuration.sh
+
+hostname=`hostname`;
+stout=${hostname}".stout"
+sterr=${hostname}".sterr"
+
+rm -Rf ${workingdir};
+mkdir ${workingdir};
+cd ${workingdir};
+cp ${scriptdir}/config.property ${workingdir};
+cp ${scriptdir}/log4j.properties ${workingdir};
+cp ${scriptdir}/myfractal.xml ${workingdir};
+cp ${scriptdir}/config/YCSB/workloads/${workloadName} ${workingdir}/workload
+
+cd ${workingdir};
+
+export CLASSPATH=${classpath}
+java -Xms1000m -Xmx2000m -XX:+UseConcMarkSweepGC me.mneri.jessy.dummy.DummyServer
