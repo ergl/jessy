75b5d1d689acad0b085ba12de50a49a850250a14
ergl <ergl@users.noreply.github.com>
2016-11-07 17:14:59 +0100

    Clean up rubis-grid5k

--- script/rubis-grid5k.sh
+++ script/rubis-grid5k.sh
@@ -1,212 +1,250 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Usage
-# ./rubis-grid5k <site1> <nclients> <nservers> <site2> <nclients> <nservers>...
+set -e
+trap "stopExecution" SIGINT SIGTERM
 
 rm -f *.fr
 running_on_grid=true
 
-if [ ${#} -lt 3 ]; then
-    echo 'grid5kLaucher: usage: this script need at least three parameters: cluster name, number of clients and number of servers'
-    exit
-fi
-
-trap "stopExecution" SIGINT SIGTERM
-
-path=$(pwd)
-
-function stopExecution {
-	echo "grid5kLaucher: grid5kLaucher stop. Deleting jobs..."
-	oargriddel $RES_ID
-    echo "grid5kLaucher: done"
-    exit
-}
-
-function reserveNodes {
-    reservationFail=true;
-    increment=0
-    next=$(date '+%Y-%m-%d %H:%M:%S')
-
-    while [ $reservationFail == "true" ]; do
-	    echo "trying to reserve nodes at... ""$next"
-	    oargridsub -w '0:30:00' $reservation -s "$next" > tmp
-
-	    # Retreving batch and grid reservation IDs
-	    RES_ID=$(grep "Grid reservation id" tmp | cut -f2 -d=)
-	    OAR_JOB_KEY_PATH=$(grep "SSH KEY" tmp | cut -b 25-)
-
-	    if [ -z "$RES_ID" ]; then
-		    increment=$(($increment+30))
-		    next=$(date '+%Y-%m-%d %H:%M:%S' --date=' +'$increment' minutes')
-		    next=${next//\"/} # Replace all the occurences of '"' with emtpy string
-		    next=${next//\'/} # Replace all the occurences of "'" with empty string
-		    echo 'clusters unavailable now, trying to reserve at... '$next
-   		    echo ""
-	    else
-		    now=$(date +%s)
-		    next=$(date -d "$next" +%s)
-		    next=${next//\"/}
-		    next=${next//\'/}
-		    timeToWait=$(( $next - $now ))
-		    minutes=$(($timeToWait / 60))
-		    echo "I will sleep for:" $minutes "minutes" #$(date -d @$timeToWait)
-		    sleep $timeToWait
-	        reservationFail=false
-	    fi
-    done
-
-    echo "done"
-}
-
-sed -ie "s#^source.*#source $path/rubis-configuration.sh#g" clauncher.sh
-sed -ie "s#^source.*#source $path/rubis-configuration.sh#g" console.sh
-sed -ie "s#^source.*#source $path/rubis-configuration.sh#g" client.sh
-sed -ie "s#^source.*#source $path/rubis-configuration.sh#g" experience.sh
-sed -ie "s#^source.*#source $path/rubis-configuration.sh#g" jessy.sh
-sed -ie "s#^source.*#source $path/rubis-configuration.sh#g" launcher.sh
-sed -ie "s#^scriptdir=.*#scriptdir=$path#g" rubis-configuration.sh
-
-export clustersNumber=$(($# / 3))
+self=${0##*/}
 export param=("$@")
 
-next=0
-i=0
-reservation="";
-next=0
+if [ ${#} -lt 3 ]; then
+  echo -e 'Usage:' ${self} '<site-name> <n-clients> <n-servers>'
+  echo 'Additionally, add more triples of site name, number of clients and numner of servers'
+  exit
+fi
 
-for i in `seq 1 $clustersNumber`; do
+# As the format is <site> <nclient> <nserver> ...
+# $# / 3 gives us the number of clusters
+export clustersNumber=$(($# / 3))
+
+# Run on ^C
+function stopExecution {
+  echo "${self}: stopping. Deleting jobs..."
+  oargriddel ${RES_ID}
+  echo "${self}: done"
+  exit
+}
+
+# Change the path to the rubis config file in all other scripts
+# s|foo|bar|g is the same as s/foo/bar/g. Use | to avoid escaping '/'
+function changeScriptPaths {
+  local path=$(pwd)
+
+  sed -ie "s|^scriptdir=.*|scriptdir=$path|g" rubis-configuration.sh
+
+  sed -ie "s|^source.*|source $path/rubis-configuration.sh|g" clauncher.sh
+  sed -ie "s|^source.*|source $path/rubis-configuration.sh|g" console.sh
+  sed -ie "s|^source.*|source $path/rubis-configuration.sh|g" client.sh
+  sed -ie "s|^source.*|source $path/rubis-configuration.sh|g" experience.sh
+  sed -ie "s|^source.*|source $path/rubis-configuration.sh|g" jessy.sh
+  sed -ie "s|^source.*|source $path/rubis-configuration.sh|g" launcher.sh
+}
+
+# Build the reservation arguments using the named sites
+function buildReservation {
+  local next=0
+  local clusters=()
+  reservation=""
+
+  for i in `seq 0 $((${clustersNumber}-1))`; do
+    # Name of the cluster
     clusters[$i]=${param[$next]}
-	nodesNumber=$((${param[$next+1]}+${param[$next+2]}))
-	#reservation="$reservation ${param[$next]}:rdef=/nodes=$nodesNumber,"
-	reservation="$reservation ${param[$next]}:rdef=/nodes=$nodesNumber/core=4,"
 
-	next=$(($next+3))
-	i=$(($i+1))
-done
+    # Number of nodes = clients + servers
+    node_number=$((${param[$next+1]}+${param[$next+2]}))
 
-reservation=${reservation#?}
-reservation=${reservation%?}
+    # Build the reservation parameters
+    # The format is different when using oargridsub, format:
+    # clusterAlias1:rdef="/nodes=1/core=2":name=nomJob:type="time‐sharing":prop="hostname = 'node1'",
+    # clusterAlias2:rdef="/nodes=2":name=nomJob2:prop="switch = 'sw3'", ...
+    reservation="$reservation ${clusters[$i]}:rdef=/nodes=$node_number/core=4,"
 
-echo "starting grid5kLaucher..."
-echo ""
-#echo "reserving nodes..."
+    # If there are more, do the same for more sites
+    next=$(($next+3))
+  done
 
-reserveNodes
+  # Trim the last (,) in the string
+  reservation=${reservation%?}
 
-#oargridsub -w '0:05:00' $reservation > tmp
-#echo "done"
-#
-#retreving batch and grid reservation IDs
-#RES_ID=$(grep "Grid reservation id" tmp | cut -f2 -d=)
-#OAR_JOB_KEY_PATH=$(grep "SSH KEY" tmp | cut -b 25-)
+  echo "Reserving at sites:"
+  printf '\t%s' "${clusters[@]}"
+  echo ""
+}
 
-rm myfractal.xml
+# Tries to actually reserve the nodes
+function reserveNodes {
+  local reservation_failure=true;
+  local increment=0
+  local next=$(date '+%Y-%m-%d %H:%M:%S')
 
-echo '<?xml version="1.0" encoding="ISO-8859-1" ?>' >> myfractal.xml
-echo '<FRACTAL>'  >> myfractal.xml
-echo '<BootstrapIdentity>' >> myfractal.xml
-echo '<nodelist>' >> myfractal.xml
+  while [[ ${reservation_failure} == "true" ]]; do
+    echo "Trying to reserve nodes at...${next}"
+    echo "With args${reservation}"
+    # Try to reserve for 30 minutes, starting now
+    oargridsub -w '0:30:00' ${reservation} -s "$next" > tmp
 
-echo 'Grid reservation id: ' $RES_ID
+    # Outputs something similar to:
+    # [OAR_GRIDSUB] [nancy] Date/TZ adjustment: 0 seconds
+    # [OAR_GRIDSUB] [nancy] Reservation success on nancy : batchId = 1091121
+    # [OAR_GRIDSUB] Grid reservation id = 56670
+    # [OAR_GRIDSUB] SSH KEY : /tmp/oargrid/oargrid_ssh_key_(...)_56670
+    #     You can use this key to connect directly to your OAR nodes with the oar user.
 
-nodeStr='' #'nodes=('
-servers='' #'servers=('
-clients='' #'clients=('
-nodes=''
+    # Retrieving the grid reservation ID
+    RES_ID=$(grep "Grid reservation id" tmp | cut -f2 -d=)
 
-j=0
-next=0
+    # Retrieve the ssh key file path
+    OAR_JOB_KEY_PATH=$(grep "SSH KEY" tmp | cut -b 25-)
 
-for i in `seq 1 $clustersNumber`; do
-    reservation="$reservation ${param[$next]}:rdef=/nodes=$nodes,"
+    # If no ID is found, command failed, try again
+    if [[ -z "$RES_ID" ]]; then
+      increment=$(($increment+30))
+      next=$(date '+%Y-%m-%d %H:%M:%S' --date=' +'${increment}' minutes')
+      # Replace all the occurences of \"\ with emtpy string
+      next=${next//\"/}
+      # Replace all the occurences of \'\ with empty string
+      next=${next//\'/}
+      echo "Clusters unavailable now, trying to reserve in ${increment} minutes."
+      echo ""
+    else
+      # If ID is found, sleep until reservation is ready
+      now=$(date +%s)
+      next=$(date -d "$next" +%s)
+      next=${next//\"/}
+      next=${next//\'/}
+      time_to_wait=$(($next - $now))
+      minutes=$(($time_to_wait / 60))
+      echo "I will sleep for:" ${minutes} "minutes"
+      sleep ${time_to_wait}
+      reservation_failure=false
+    fi
+  done
 
-	nodeName=${param[$next]}
-	serverNumber=${param[$next+1]}
-	clientNumber=${param[$next+2]}
+  export OAR_JOB_KEY_FILE=${OAR_JOB_KEY_PATH}
+  echo 'Exported oarJobKeyFile ' ${OAR_JOB_KEY_PATH}
 
-	echo ""
-	echo "**********************"
-	echo "* deploy on "$nodeName" *"
-	echo "**********************"
-	echo "server: "$serverNumber
-	echo "client: "$clientNumber
-	echo ""
+  echo "Done"
+}
 
-	oargridstat -w -l $RES_ID -c $nodeName | sed '/^$/d' | sort | uniq > ./machines
+function buildFractal {
+  local reservation_id=${RES_ID}
+  rm myfractal.xml
+
+  echo '<?xml version="1.0" encoding="ISO-8859-1" ?>' >> myfractal.xml
+  echo '<FRACTAL>'  >> myfractal.xml
+  echo '<BootstrapIdentity>' >> myfractal.xml
+  echo '<nodelist>' >> myfractal.xml
+
+  echo 'Grid reservation id: ' ${reservation_id}
+
+  local nodeStr=''
+  local servers=''
+  local clients=''
+  local nodes=''
+
+  local j=0
+  local next=0
+  for i in `seq 0 $((${clustersNumber}-1))`; do
+    nodeName=${param[$next]}
+    serverNumber=${param[$next+1]}
+    clientNumber=${param[$next+2]}
+
+    echo ""
+    echo "**********************"
+    echo "* deploy on "${nodeName}" *"
+    echo "**********************"
+    echo "server: "${serverNumber}
+    echo "client: "${clientNumber}
+    echo ""
+
+    oargridstat -w -l ${RES_ID} -c ${nodeName} | sed '/^$/d' | sort | uniq > ./machines
+
+    local k=0
+    local next=$(($next+3))
+    while read line; do
+      host ${line} > tmp
+      local name=$(cut tmp -f1 -d ' ')
+      local ip=$(cut tmp -f4 -d ' ')
+
+      nodes="$nodes \"$name\""
+
+      if [[ ${k} -lt ${serverNumber} ]]; then
+        echo 'server: '${name}
+        echo '<node id="'${j}'" ip="'${ip}'"/>' >> myfractal.xml
+        servers="$servers \"$name\""
+      else
+        echo 'client: '${name}
+        clients="$clients \"$name\""
+      fi
+
+      j=$((j+1))
+      k=$((k+1))
+    done < machines
+  done
+
+  echo '</nodelist>' >> myfractal.xml
+  echo '</BootstrapIdentity>' >> myfractal.xml
+  echo '</FRACTAL>' >> myfractal.xml
+
+  echo ""
+
+  nodeStr="nodes=("${nodes}")"
+  servers="servers=("${servers}")"
+  clients="clients=("${clients}")"
+
+  # Shuffle the ips around
+  ./shufflemyfractal.sh
+
+  echo "Fractal configuration file is done"
+
+  sed -ie "s|nodes=.*|${nodeStr}|g" rubis-configuration.sh
+  sed -ie "s|servers=.*|${servers}|g" rubis-configuration.sh
+  sed -ie "s|clients=.*|${clients}|g" rubis-configuration.sh
+
+  rm machines tmp
+
+  echo "rubis-configuration.sh file is done"
+}
+
+function syncCode {
+  echo 'Synchronizing keys and data...'
+  local next=0
+
+  # Sync with all the provided sites
+  for i in `seq 0 $((${clustersNumber}-1))`; do
+    nodeName=${param[$next]}
+    echo "Synchronizing${nodeName}..."
+
+    rsync -a -f"+ */" -f"- *" ../../jessy/script ${nodeName}.grid5000.fr:~/jessy
+    rsync --delete -az ./* ${nodeName}.grid5000.fr:~/jessy/script/
 
     next=$(($next+3))
-	k=0
+  done
+}
 
-	while read line; do
-		host $line > tmp
-		name=$(cut tmp -f1 -d ' ')
-		ip=$(cut tmp -f4 -d ' ')
- 
-		nodes="$nodes \"$name\""
+function launchExperience {
+  echo ""
+  echo "grid5kLaucher: myfractal and configuration.sh are done"
+  echo -e "\tLaunching experience..."
+  ./rubis-experience.sh ${param[*]}
+}
 
-		if [ $k -lt $serverNumber ]; then
-			echo 'server: '$name
-   			echo '<node id="'$j'" ip="'$ip'"/>' >> myfractal.xml
-			servers="$servers \"$name\""
-		else
-		    echo 'client: '$name
-		    clients="$clients \"$name\""
-		fi
+function endScript {
+  echo "grid5kLaucher: done, deleting jobs"
+  oargriddel ${RES_ID}
+}
 
-		j=$((j+1))
-		k=$((k+1))
-	done < machines
-done
+function run {
+  echo "starting grid5kLaucher..."
+  changeScriptPaths
+  buildReservation
+  reserveNodes
+  buildFractal
+  syncCode
+  launchExperience
+  endScript
+}
 
-echo ""
-
-nodeStr="nodes=("$nodes")"
-servers="servers=("$servers")"
-clients="clients=("$clients")"
-
-echo '</nodelist>' >> myfractal.xml
-echo '</BootstrapIdentity>' >> myfractal.xml
-echo '</FRACTAL>' >> myfractal.xml
-
-
-./shufflemyfractal.sh
-
-
-echo "fractal configuration file is done"
-
-sed -i "s/nodes=.*/${nodeStr}/g" rubis-configuration.sh
-sed -i "s/servers=.*/${servers}/g" rubis-configuration.sh
-sed -i "s/clients=.*/${clients}/g" rubis-configuration.sh
-echo "rubis-configuration.sh file is done"
-
-rm machines tmp
-
-export OAR_JOB_KEY_FILE=$OAR_JOB_KEY_PATH
-
-echo 'exported oarJobKeyFile ' $OAR_JOB_KEY_PATH
-
-next=0
-echo 'synchronizing keys and data...'
-for i in `seq 1 $clustersNumber`; do
-	nodeName=${param[$next]}
-	echo "synchronizing "$nodeName"..."
-
-	rsync -a -f"+ */" -f"- *" ../../jessy/script $nodeName.grid5000.fr:~/jessy
-
-	rsync --delete -az ./* $nodeName.grid5000.fr:~/jessy/script/
-
-	next=$(($next+3))
-done
-
-
-echo ""
-echo "**************************************************************************************"
-echo "*** grid5kLaucher: myfractal and configuration.sh are done, launching experience... ***"
-echo "**************************************************************************************"
-
-./rubis-experience.sh ${param[*]}
-echo "******************************************************************************"
-echo "grid5kLaucher: done, deleting jobs"
-echo "******************************************************************************"
-
-oargriddel $RES_ID
+run
